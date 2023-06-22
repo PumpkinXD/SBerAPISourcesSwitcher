b4f2ed6c0c3b6459901432298030f27d9a846f53
PumpkinXD <1042588515@qq.com>
2023-06-22 18:16:06 +0800

    clean up, change lbin source and renaming

--- src/main/java/io/github/pumpkinxd/sberapisourcesswitcher/mixins/skyblocker/mixinPriceInfoTooltip_apiSwitch.java
+++ src/main/java/io/github/pumpkinxd/sberapisourcesswitcher/mixins/skyblocker/mixinPriceInfoTooltip_apiSwitch.java
@@ -36,51 +36,20 @@ public class mixinPriceInfoTooltip_apiSwitch {
 
     @Dynamic
     @Inject(method = "downloadPrices",at=@At("HEAD"),cancellable = true,remap = false)
-    private static void func(String type, CallbackInfoReturnable<JsonObject> ci){
+    private static void func(String type, CallbackInfoReturnable<JsonObject> cir){
         if(type.matches("lowest bins"))
         {
             try{
-                String lbinurl="https://hysky.de/api/auctions/lowestbins";
+                //String lbinurl="https://hysky.de/api/auctions/lowestbins";//not available in my region now
+                String lbinurl="https://moulberry.codes/lowestbin.json.gz";
                 URL apiAddress = new URL(lbinurl);
                 InputStream src = apiAddress.openStream();
                 InputStreamReader reader = new InputStreamReader(lbinurl.contains(".gz") ? new GZIPInputStream(src) : src);
-                ci.setReturnValue(new Gson().fromJson(reader, JsonObject.class));
+                cir.setReturnValue(new Gson().fromJson(reader, JsonObject.class));
             }catch (Throwable e){}
 
         }
 
     }
-//    @Dynamic("Skyblocker")
-//    @Inject(method = "<clinit>", at = @At(value="HEAD",remap = false),remap = false)
-//    private void inj(){}
-
-
-
-//    /***
-//     * crashs with Mixin apply for mod sberapisourcesswitcher failed #sberapisourcesswitcher:modid.mixins.json:skyblocker.mixinPriceInfoTooltip_apiSwitch from mod sberapisourcesswitcher -> me.xmrvizzy.skyblocker.skyblock.item.PriceInfoTooltip: org.spongepowered.asm.mixin.injection.throwables.InvalidInjectionException non-static callback method me/xmrvizzy/skyblocker/skyblock/item...
-//     * @see "https://mclo.gs/FuEzTml"
-//     *
-//     * For inject point
-//     * @see "http://github.com/SkyblockerMod/Skyblocker/blob/b75a2c66ead760bd2471fec16f6abd6303271269/src/main/java/me/xmrvizzy/skyblocker/skyblock/item/PriceInfoTooltip.java#LL354C78-L354C78"
-//     *
-//     */
-    /*
-    @Dynamic("Skyblocker")
-    @ModifyArg(
-            method = "<clinit>",
-
-            at=@At(
-                    value = "INVOKE",
-                    //target="Ljava/util/Map;put(Ljava/lang/String;Ljava/lang/String)Ljava/lang/String", //javac says Invalid descriptor, even ending with ";"
-                    target="put",
-                    ordinal = 3,
-                    remap = false
-                            ),
-            remap = false,
-            index=1
-        )
-private static String func(String arg){return "https://hysky.de/api/auctions/lowestbins";}
-
-*/
 
 }
